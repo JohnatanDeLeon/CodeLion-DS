7ed9e082f7a59b04e4188819c0cee44864eb691d
Johnatan De Leon <muza_2303@hotmail.com>
2025-08-15 19:36:25 -0600

    feat: add test workflow utility function
    This new utility function provides a real code change that should
    trigger the changesets system to actually publish a new version.
    
    Expected: Release job should publish and Deploy Storybook should execute.

---
+++ src/utils/test-workflow.ts
@@ -0,0 +1,9 @@
+/**
+ * Utility function added for workflow testing
+ * This change should trigger the changesets to actually publish
+ */
+export const testWorkflow = () => {
+  return 'Workflow test successful!';
+};
+
+export default testWorkflow;
