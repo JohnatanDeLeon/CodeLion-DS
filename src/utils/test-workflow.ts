3abb51d4d37d3fc18da85cf857062a473f9f5478
Johnatan De Leon <muza_2303@hotmail.com>
2025-08-15 19:39:54 -0600

    fix: apply linting rules to test-workflow utility
    Fixed Prettier error: changed single quotes to double quotes
    as required by the project's linting configuration.
    
    This should allow the workflow to complete successfully.

--- src/utils/test-workflow.ts
+++ src/utils/test-workflow.ts
@@ -3,7 +3,7 @@
  * This change should trigger the changesets to actually publish
  */
 export const testWorkflow = () => {
-  return 'Workflow test successful!';
+  return "Workflow test successful!";
 };
 
 export default testWorkflow;
